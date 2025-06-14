b0b1277cbe7a69e3aa8b6e641079e9ce36f74b05
Canon Heise <canon.heise@gmail.com>
2025-06-14 16:46:56 -0600

    ScrollWheel Success

--- Checkbook.py
+++ Checkbook.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
-import customtkinter
 import os
 import string, csv
 
@@ -10,11 +9,16 @@ class Checkbook_Window(tk.Tk):
         # setup
         super().__init__()
         self.title(title)
-        self.geometry("1000x450")
-        self.minsize(1000,450)
+        self.geometry("900x450")
+        self.minsize(900,450)
+        self.maxsize(900,450)
+        
+        # Temp Values
+        data = [("Megan","Sorro"), ("Charlie", "Gregory"),("Megan","Sorro"), ("Charlie", "Gregory"),("Megan","Sorro"), ("Charlie", "Gregory"),("Megan","Sorro"), ("Charlie", "Gregory"),("Megan","Sorro"), ("Charlie", "Gregory"),("Megan","Sorro"), ("Charlie", "Gregory"),("Megan","Sorro"), ("Charlie", "Gregory"),("Megan","Sorro"), ("Charlie", "Gregory"),("Megan","Sorro"), ("Charlie", "Gregory"),("Megan","Sorro"), ("Charlie", "Gregory"),("Megan","Sorro"), ("Charlie", "Gregory")]
 
         # widgets
         self.menu = Core_Window(self)
+        self.check = ListFrame(self, data , 50)
 
         # run
         self.mainloop()
@@ -22,32 +26,32 @@ class Checkbook_Window(tk.Tk):
 class Core_Window(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
-        self.place(x=0, y=0, relwidth=1, relheight=1)
+        self.place(x=0, y=0, relwidth=1, relheight=0.3)
         self.create_widgets()
 
     def create_widgets(self):
         # Setup information
         xAxis = ["Flag", "Number", "Information", "Date", "Status", "Amount", "Deposit", "Balance"]
         
-        # buttons 
+        # buttons/labels
+        FileName = tk.Label(self, text = "input", background = "white" )
         AddButton = tk.Button(self, text="Add Row", command = self.rowAdd)
         SaveButton = tk.Button(self, text="Save", command = self.saveApp)
-        Menu = ListFrame(self, "x", 2)
 
         # Creates grid
-        self.columnconfigure((0,1,2,3,4,5,6,7,8), weight = 1, uniform = "a")
-        self.rowconfigure((0,1,2,3,4), weight = 1, uniform = "a")
+        self.columnconfigure((0,1,2,3,4,5,6,7), weight = 1, uniform = "a")
+        self.rowconfigure((0,1,2,3), weight = 1, uniform = "a")
 
         # Places Widgest
+        FileName.grid(row = 0, column = 0)
         for x in xAxis:
             text = tk.Label(self, text = x, width=15, background = "white")
-            text.grid(row = 0, column = xAxis.index(x))
-        Menu.grid(column = 0, row = 1, rowspan=3)
-        AddButton.grid(column=1, row = 4)
-        SaveButton.grid(column=2, row = 4)
+            text.grid(row = 2, column = xAxis.index(x))
+        AddButton.grid(column=0, row = 1)
+        SaveButton.grid(column=1, row = 1)
     
     def rowAdd():
-        setting+=1
+        
         return
     def saveApp():
         '''
@@ -67,15 +71,56 @@ class Core_Window(ttk.Frame):
 class ListFrame(ttk.Frame):
     def __init__(self, parent, text_data, item_height):
         super().__init__(master = parent)
-        self.pack(expand = True, fill = "both")
+        self.place(x=0, y=100, relwidth=1, relheight=0.75)
 
         self.text_data = text_data
         self.item_number = len(text_data)
         self.list_height = self.item_number * item_height
 
-        self.canvas = tk.Canvas(self, background = "white")
-        self.canvas.pack(expand = True, fill = "both")
+        self.canvas = tk.Canvas(self, background = "white", scrollregion = (0,0,900,self.list_height))
+        self.canvas.place(x=0, y=0, relwidth=1, relheight=1)
 
+        # display frame
+        self.frame = ttk.Frame(self)
+        for index, item in enumerate(self.text_data):
+            self.create_item(index, item).pack(expand = True, fill = 'both', pady = 1, padx=5)
+
+        # scrollbar
+        self.scrollbar = ttk.Scrollbar(self, orient = 'vertical', command = self.canvas.yview)
+        self.canvas.configure(yscrollcommand = self.scrollbar.set)
+        self.scrollbar.place(relx=1, rely=0, relheight = 1, anchor = 'ne')
+
+
+        #events
+        self.canvas.bind_all('<MouseWheel>', lambda event: self.canvas.yview_scroll(-int(event.delta / 60),"units"))
+        self.bind('<Configure>', self.update_size)
+
+
+    def update_size(self, event):
+        if self.list_height >= self.winfo_height():
+            xheight = self.list_height
+        else:
+            xheight = self.winfo_height
+            self.canvas.unbind_all('<MouseWheel>')
+        self.canvas.create_window((0,0), window = self.frame, anchor = "nw", width = 900, height = xheight)
+
+
+    def create_item(self, index, item):
+        frame = ttk.Frame(self.frame)
+        index +=1
+
+        # Grid layout
+        frame.rowconfigure(0, weight = 1)
+        frame.columnconfigure((0,1,2,3,4,5,6,7), weight = 1, uniform='a')
+
+        #widgets
+        ttk.Label(frame, text = f'{index}', width=111).grid(row=0, column=0)
+        ttk.Label(frame, text = f'{item[0]}', width=111).grid(row=0, column=1)
+        ttk.Entry(frame, text = f"{item[1]}", width=111).grid(row=0, column=2)
+
+        return frame
+
+        
 
 title = "F.A.F.C"
 Checkbook_Window(title)
